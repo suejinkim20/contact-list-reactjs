575027fd3dd8cd34b95dd9f84ee0556b82ad9690
Suejin Kim <kim.suejin.20@gmail.com>
2021-08-18 17:34:48 -0400

    populated contact card with api data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import useFetch from 'react-fetch-hook'
 import ContactCards from './components/ContactCards'
 
 const App = () => {
   const url = 'https://randomuser.me/api/'
-  const { isLoading, data, error } = useFetch(url+'?results=40')
-  data && console.log(data)
+  const { isLoading, data, error } = useFetch(url+'?nat=us&results=20')
 
+  const [contactList, setContactList] = useState('')
+  const [filterQuery, setFilterQuery] = useState('')
+
+  useEffect(() => {
+    if (filterQuery) {
+      // use filterQuery function
+    } else {
+      // display all the data
+      setContactList(data?.results)
+    }
+  }, [data, filterQuery])
 
   return (
-    <div className={'bg-gray-700'}>
+    <div className={'bg-gray-700 font-sans tracking-wide'}>
       <section>
         <form>
           <input
@@ -20,7 +31,7 @@ const App = () => {
       </section>
 
       <section className={'grid sm:grid-cols-2 lg:grid-cols-4 gap-6 p-20'}>
-        <ContactCards />
+        <ContactCards contactList={contactList} />
       </section>
     </div>
   );
--- src/components/ContactCards.js
+++ src/components/ContactCards.js
@@ -1,160 +1,38 @@
-const ContactCards = () => {
+import React from 'react';
+
+const ContactCards = ({ contactList }) => {
+    // contactList && console.log(contactList)
+    if (!contactList) {
+        return <h2> no users reported</h2>
+    } else {
+    
     return (
         <>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
-            <figure className='bg-white h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    alt='user'
-                    className='w-32 h-32 rounded-full mx-auto'
-                    src='https://randomuser.me/api/portraits/women/40.jpg' />
-                <figcaption className='text-center mt-5'>
-                    <p className='text-grey-700 font-semibold text-xl mb-2'>
-                        name
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>email:</span> contact.email
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>address:</span> contact.address
-                    </p>
-                    <p className='text-gray-500'>
-                        <span className='font-medium'>phone:</span> contact.phone</p>
-                </figcaption>
-            </figure>
+            {contactList?.map((contact, index) => (
+                <figure key={index} className='bg-white h-100 rounded-lg shadow-md pt-7 pb-3'>
+                    <img
+                        alt='user'
+                        className='w-32 h-32 rounded-full mx-auto'
+                        src={contact.picture.large} />
+                    <figcaption className='text-center mt-5'>
+                        <p className='text-grey-600 font-semibold text-xl mb-2'>
+                            {contact.name.first} {contact.name.last}
+                        </p>
+                        <p className='text-gray-500'>
+                            <span className='font-medium'>email:</span> {contact.email}
+                        </p>
+                        <p className='text-gray-500'>
+                        <span className='font-medium'>address:</span> {contact.location.street.number} {contact.location.street.name}
+                        </p>
+                        <p className='text-gray-500'>{contact.location.city}, {contact.location.state} </p>
+                        <p className='text-gray-500'>
+                    <span className='font-medium'>phone:</span> {contact.phone}</p>
+                    </figcaption>
+                </figure>
+
+            ))}
         </>
-    )
+    )}
 }
 
 export default ContactCards
